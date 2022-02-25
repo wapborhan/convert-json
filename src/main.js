5301bc32707fbe344f05fee447c4936b72d02f9c
Borhan Uddin <borhaninfos@gmail.com>
2022-02-25 08:11:48 +0600

    object field add

--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 function convertToJSON() {
+  var object = document.getElementById("object").value;
   var firstname = document.getElementById("firstname").value;
   var lastname = document.getElementById("lastname").value;
   var email = document.getElementById("email").value;
 
   var jsonObject = {
-    FirstName: firstname,
-    LastName: lastname,
-    email: email,
+    object: {
+      FirstName: firstname,
+      LastName: lastname,
+      email: email,
+    },
   };
 
   editor.setValue(JSON.stringify(jsonObject, null, "\t"));
 }
 
+// Download JSON
 function saveToFile() {
   convertToJSON();
   var jsonObjectAsString = editor.getValue();
 
   var blob = new Blob([jsonObjectAsString], {
-    //type: 'application/json'
-    type: "octet/stream",
+    type: "application/json",
+    // type: "octet/stream",
   });
-  console.log(blob);
+  // console.log(blob);
 
   var anchor = document.createElement("a");
   anchor.download = "Download.json";
@@ -33,7 +37,7 @@ function saveToFile() {
   document.getElementById("editor").append(anchor);
 }
 
-// generatePDF
+// Generate PDF
 function generatePDF() {
   // var element = editor.getValue();
   const element = document.getElementById("card");
@@ -42,12 +46,10 @@ function generatePDF() {
 
 // Editor
 var editor = ace.edit("editor");
-editor.session.setMode("ace/mode/json");
+// editor.session.setMode("ace/mode/json");
 editor.getSession().setMode("ace/mode/json");
 editor.setTheme("ace/theme/xcode");
-editor.renderer.setShowGutter();
 editor.session.setTabSize(4);
-
 editor.find("needle", {
   backwards: false,
   wrap: false,
@@ -66,12 +68,9 @@ editor.findPrevious();
 //     "data:text/plain;charset=utf-8," + encodeURIComponent(text)
 //   );
 //   element.setAttribute("download", filename);
-
 //   element.style.display = "none";
 //   document.body.appendChild(element);
-
 //   element.click();
-
 //   document.body.removeChild(element);
 // }
 // editor.change();
